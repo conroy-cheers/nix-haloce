7970de4e5a6ac27c8875ecebb269577c8356bd7d
Conroy Cheers <conroy@corncheese.org>
2026-03-04 13:37:01 +1100

    fix: add default package

--- packages/default.nix
+++ packages/default.nix
@@ -2,11 +2,11 @@
   pkgs,
   nix-overlayfs,
   overlayfsLib,
-}:
-{
+}: rec {
   halo-custom-edition = pkgs.callPackage ./halo-custom-edition {
     inherit (nix-overlayfs) wineGeWin32Modules;
     inherit (nix-overlayfs) wineTkgWow64Modules;
     inherit overlayfsLib;
   };
+  default = halo-custom-edition;
 }
